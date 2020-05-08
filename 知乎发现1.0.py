8fb9f2e207ad77edb248cb97707cdc28105326dd
ZeroDuck <61463064+ZeroDuck@users.noreply.github.com>
2020-05-08 22:47:37 +0800

    Update 知乎发现1.0.py
    应该是知乎网页改动过

--- 知乎发现1.0.py
+++ 知乎发现1.0.py
@@ -1,11 +1,15 @@
 import requests
 from pyquery import PyQuery as pq
 
-url = 'https://www.zhihu.com/explore'
+url = 'https://weibo.com/'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362 '
 }
 html = requests.get(url,headers=headers).text
-#doc = pq(url=url)
-print(html,'\n',"*"*200)
-#print(doc)
+doc = pq(html)
+print(doc,'\n','\n')
+items = doc('.explore-tab .feed-item').items()
+for item in items:
+    question = item.find('h2').text()
+    print(question)
